d0e848a8113aedab8a200c05da2c1c4d5f0df504
pavel <ppyakov@gmail.com>
2017-11-08 19:07:05 +0300

    next step

--- lesson_15.php
+++ lesson_15.php
@@ -1,7 +1,98 @@
 <?php
 $pdo = new PDO('mysql:host=localhost;dbname=global;charset=UTF8', 'root', 'qwerty');
+
 date_default_timezone_set('Europe/Moscow');
 $time = date("Y-m-d H:m:s");
+
+/*
+Функция отрисовывает таблицу
+*/
+function getTable($row){
+	if(empty($row)){
+		return '<tr><td></td><td></td><td id="center"></td><td></td><td></td><td></td><td></td></tr>';
+	}
+	else{
+		if($row['is_done'] == 0){
+			$task_status = '<span style="color: orange;">В процессе</span>';
+		}
+		else{
+			$task_status = '<span style="color: green;">Выполнено</span>';
+		}
+		
+		return '<tr>'.'<td>'.$row['description'].'</td>'.'<td>'.$row['date_added'].'</td>'.'<td id="center">'.$task_status.'</td>'.'<td>'.'<a href=?id='.$row['id'].'&action=edit>Изменить</a>'.' '.'<a href=?id='.$row['id'].'&action=done>Выполнить</a>'.' '.'<a href=?id='.$row['id'].'&action=delete>Удалить</a>'.'</td>'.'<td>'.assignUser($row['id'], $row['assigned_user_id']).'</td>'.'<td>'.getUserName($row['user_id']).'</td>'.'<td><form method="GET"><select name="assigned_user">'.getListUsers().'</select><input type="submit" name="assign" value="Назначить" /></form></td>'.'</tr>';
+	}
+}
+
+/*
+Функция отображает логин пользователя
+*/
+function getUserName($row_user_id){
+	$pdo = new PDO('mysql:host=localhost;dbname=global;charset=UTF8', 'root', 'qwerty');
+	
+	$sql_select_user = $pdo->prepare('SELECT id FROM user WHERE login = :user');
+	$sql_select_user->bindParam(':user', $_SESSION['user']);
+	$sql_select_user->execute();
+	$user_id = $sql_select_user->fetchColumn();
+	
+	if($user_id == $row_user_id){
+		return 'Вы';
+	}
+	else{
+		$sql_select_login = $pdo->prepare('SELECT login FROM user WHERE id = :user_id');
+		$sql_select_login->bindParam(':user_id', $row_user_id);
+		$sql_select_login->execute();
+		$user_login = $sql_select_login->fetchColumn();
+		return $user_login;
+	}
+}
+
+/*
+Функция отрисовывает выпадающий список пользователей
+*/
+function getListUsers(){
+	$pdo = new PDO('mysql:host=localhost;dbname=global;charset=UTF8', 'root', 'qwerty');
+	$sql_list_users = $pdo->prepare('SELECT * FROM user');
+	$sql_list_users->execute();
+	$data = $sql_list_users->fetchAll();
+	$list_str ='';
+	
+	foreach ($data as $row) {
+		$list_str = $list_str.'<option selected value="'.$row["login"].'">'.$row["login"].'</option>';
+	}
+	
+	return $list_str;
+}
+
+/*
+Функция назначает пользователя
+*/
+function assignUser($row_id, $row_assigned_user_id){
+	if(empty($_GET['assigned_user'])){
+		return getUserName($row_assigned_user_id);
+	}
+	else{
+		$pdo = new PDO('mysql:host=localhost;dbname=global;charset=UTF8', 'root', 'qwerty');
+		
+		$sql_user_id = $pdo->prepare('SELECT id FROM user WHERE login = :user_login');
+		$sql_user_id->bindParam(':user_login', $_GET['assigned_user']);
+		$sql_user_id->execute();
+		$user_id = $sql_user_id->fetchColumn();
+		
+		$sql = 'UPDATE task SET assigned_user_id = :assigned_user_id WHERE id= :row_id';
+		$row_id = $row_id;
+		$assigned_user_id = $user_id;
+		$stmt = $pdo->prepare($sql);
+		$stmt->bindParam(':row_id', $row_id);
+		$stmt->bindParam(':assigned_user_id', $assigned_user_id, PDO::PARAM_INT);	
+		$stmt->execute();
+		
+		$sql_select_login = $pdo->prepare('SELECT assigned_user_id FROM task WHERE id = :task_id');
+		$sql_select_login->bindParam(':task_id', $row_id);
+		$sql_select_login->execute();
+		$user_login = $sql_select_login->fetchColumn();
+		return getUserName($user_login);
+	}
+}
 
 if(empty($_GET['action'])){
 	
@@ -21,21 +112,47 @@ else if($_GET['action'] == 'done'){
 	$stmt->execute();
 }
 
-function getTable($row){
-	if(empty($row)){
-		return '<tr><td></td><td></td><td id="center"></td><td></td><td></td><td></td><td></td></tr>';
+if(!empty($_POST['description'])){
+	if($_POST['save'] == 'Добавить'){
+		
+		$sql_select_user = $pdo->prepare('SELECT id FROM user WHERE login = :user');
+		$sql_select_user->bindParam(':user', $_SESSION['user']);
+		$sql_select_user->execute();
+		
+		$sql_insert = $pdo->prepare('INSERT INTO task(user_id, assigned_user_id, description, is_done, date_added) VALUES (:user_id, :assigned_user_id, :task_description, :is_done, :time_now)');		
+		$sql_insert->bindParam(':user_id', $user_id);
+		$sql_insert->bindParam(':assigned_user_id', $assigned_user_id);
+		$sql_insert->bindParam(':task_description', $task_desctiption);
+		$sql_insert->bindParam(':time_now', $time_now);
+		$sql_insert->bindParam(':is_done', $done);
+		
+
+		$user_id = $sql_select_user->fetchColumn();
+		$assigned_user_id = $user_id;
+		$task_desctiption = strip_tags($_POST['description']);
+		$time_now = $time;
+		$done = '0';
+		$inserted = $sql_insert->execute();
+		header('Location: lesson_15.php');
 	}
-	else{
-		if($row['is_done'] == 0){
-			$task_status = '<span style="color: orange;">В процессе</span>';
-		}
-		else{
-			$task_status = '<span style="color: green;">Выполнено</span>';
-		}
-		return '<tr>'.'<td>'.$row['description'].'</td>'.'<td>'.$row['date_added'].'</td>'.'<td id="center">'.$task_status.'</td>'.'<td>'.'<a href=?id='.$row['id'].'&action=edit>Изменить</a>'.' '.'<a href=?id='.$row['id'].'&action=done>Выполнить</a>'.' '.'<a href=?id='.$row['id'].'&action=delete>Удалить</a>'.'</td>'.'<td>'.$row['assigned_user_id'].'</td>'.'<td>'.$row['user_id'].'</td>'.'<td>'.$row['assigned_user_id'].'</td>'.'</tr>';
+	else if($_POST['save'] == 'Сохранить'){
+			
+		$sql_insert = $pdo->prepare('UPDATE task SET description = :task_description, is_done = :is_done, date_added = :time_now WHERE id = :task_id');
+		
+		$sql_insert->bindParam(':task_id', $task_id);
+		$sql_insert->bindParam(':task_description', $task_desctiption);
+		$sql_insert->bindParam(':task_description', $task_desctiption);
+		$sql_insert->bindParam(':time_now', $time_now);
+		$sql_insert->bindParam(':is_done', $done);
+		
+		$task_id = strip_tags($_GET['id']);
+		$task_desctiption = strip_tags($_POST['description']);
+		$time_now = $time;
+		$done = '0';
+		$inserted = $sql_insert->execute();
+		header('Location: lesson_15.php');
 	}
 }
-
 ?>
 <html> 
 <head>
@@ -105,65 +222,13 @@ function getTable($row){
         <input type="submit" name="sort" value="Отсортировать" />
     </form>
 </div>
-<?php
-if(!empty($_POST['description'])){
-	if($_POST['save'] == 'Добавить'){
-		
-		$sql_select_user = $pdo->prepare('SELECT id FROM user WHERE login = :user');
-		echo'<br>';
-		var_dump($sql_select_user);
-		echo'<br>';
-		$sql_select_user->bindParam(':user', $_SESSION['user']);
-		echo'<br>';
-		var_dump($sql_select_user);
-		echo'<br>';
-		
-		$sql_insert = $pdo->prepare('INSERT INTO task(user_id, assigned_user_id, description, is_done, date_added) VALUES (:user_id, :assigned_user_id, :task_description, :is_done, :time_now)');		
-		$sql_insert->bindParam(':user_id', $user_id);
-		$sql_insert->bindParam(':assigned_user_id', $assigned_user_id);
-		$sql_insert->bindParam(':task_description', $task_desctiption);
-		$sql_insert->bindParam(':time_now', $time_now);
-		$sql_insert->bindParam(':is_done', $done);
-		
-
-		$user_id = $sql_select_user->execute();
-		echo'<br>';
-		var_dump($user_id);
-		echo'<br>';
-		
-		$assigned_user_id = $user_id;
-		$task_desctiption = strip_tags($_POST['description']);
-		$time_now = $time;
-		$done = '0';
-		$inserted = $sql_insert->execute();
-		header('Location: lesson_15.php');
-	}
-	else if($_POST['save'] == 'Сохранить'){
-			
-		$sql_insert = $pdo->prepare('UPDATE task SET description = :task_description, is_done = :is_done, date_added = :time_now WHERE id = :task_id');
-		
-		$sql_insert->bindParam(':task_id', $task_id);
-		$sql_insert->bindParam(':task_description', $task_desctiption);
-		$sql_insert->bindParam(':task_description', $task_desctiption);
-		$sql_insert->bindParam(':time_now', $time_now);
-		$sql_insert->bindParam(':is_done', $done);
-		
-		$task_id = strip_tags($_GET['id']);
-		$task_desctiption = strip_tags($_POST['description']);
-		$time_now = $time;
-		$done = '0';
-		$inserted = $sql_insert->execute();
-		header('Location: lesson_15.php');
-	}
-}
-?>
 <div style="clear: both"></div>
 <table>
     <tr>
         <th>Описание задачи</th>
         <th>Дата добавления</th>
         <th>Статус</th>
-        <th></th>
+        <th>Действия</th>
 		<th>Ответственный</th>
 		<th>Автор</th>
 		<th>Закрепить задачу за пользователем</th>
@@ -190,8 +255,7 @@ if(!empty($_GET['sort_by'])){
 }
 else{
 	$sql = 'SELECT * FROM task ORDER BY date_added';
-	
-	var_dump($sql);
+
 	foreach ($pdo->query($sql) as $row) {
 		echo getTable($row);
 	}	
--- register.php
+++ register.php
@@ -14,18 +14,47 @@ $pdo = new PDO('mysql:host=localhost;dbname=global;charset=UTF8', 'root', 'qwert
 </form>
 
 <?php
-if(!empty($_POST['login'])){
+if(!empty($_POST['login'])){	
+	$sql_select_user = $pdo->prepare('SELECT login FROM user');
+	$sql_select_user->execute();
+	$users_login = $sql_select_user->fetchAll(PDO::FETCH_COLUMN);
+	
 	if($_POST['register'] == 'Регистрация'){
-		$sql_insert = $pdo->prepare('INSERT INTO `user`(`login`, `password`) VALUES (:login, :password)');
-		$sql_insert->bindParam(':login', $_POST['login']);
-		$sql_insert->bindParam(':password', md5($_POST['password']));
-		$inserted = $sql_insert->execute();
-		$_SESSION['user'] = $_POST['login'];
-		header('Location: lesson_15.php');
+		$login = strval($_POST['login']);
+		
+		if (in_array($_POST['login'], $users_login)) {
+			echo "Такой логин уже есть в базе! Введит логин и пароль и нажмите 'Вход'.";
+		}
+		else{
+			$sql_insert = $pdo->prepare('INSERT INTO user(login, password) VALUES (:login, :password)');
+			$sql_insert->bindParam(':login', $_POST['login']);
+			$sql_insert->bindParam(':password', md5($_POST['password']));
+			$inserted = $sql_insert->execute();
+			$_SESSION['user'] = $_POST['login'];
+			header('Location: lesson_15.php');
+		}
 	}
 }
 else{
 	echo '<br>Необходимо зарегистрироваться! Заполните все поля.';
+}
+
+if(!empty($_POST['sign_in'])){
+	if($_POST['sign_in'] == 'Вход'){
+		$sql_select_user = $pdo->prepare('SELECT * FROM user WHERE login = :user_login LIMIT 1');
+		$sql_select_user->bindParam(':user_login', $_POST['login']);
+		$sql_select_user->execute();
+		$user_db_pass = $sql_select_user->fetchAll(PDO::FETCH_COLUMN, 2);
+
+		$user_password = md5($_POST['password']);
+
+		if($user_db_pass == $user_password){
+			header('Location: lesson_15.php');
+		}
+		else{
+			header('Location: register.php');
+		}
+	}
 }
 ?>
 </body>
